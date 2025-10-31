0e8063463bb7dc2936f060102b655518a7257a3a
RafaMiura <143235499+RafaMiura@users.noreply.github.com>
2025-10-31 01:48:47 +0000

    feat: delete product

--- server/run-integration-tests.sh
+++ server/run-integration-tests.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-sudo apt update 
-sudo apt install -y jq
+apt update
+apt install -y jq
 
 HTTP_STATUS=$(
     curl -X 'POST' \
@@ -16,16 +16,27 @@ HTTP_STATUS=$(
     "nome": "Uva"
     }'
 )
-
+echo "Status HTTP: $HTTP_STATUS"
 if [ "$HTTP_STATUS" -ne 201 ]; then
     echo "Erro ao criar produto"
     exit 1  
 fi
 
-PRODUTO_ID=$(jq -r '.id' product_create.json)
+PRODUTO_ID=$(jq '.id' product_create.json)
+
 echo "Produto criado com ID: $PRODUTO_ID"
 
+HTTP_STATUS=$(
+    curl -X 'DELETE' \
+    "http://localhost:9000/api/v2/produtos/$PRODUTO_ID" \
+    -H 'accept: */*' \
+    -w "%{http_code}"
+)
 echo "Status HTTP: $HTTP_STATUS"
+if [ "$HTTP_STATUS" -ne 204 ]; then
+    echo "Erro ao deletar produto"
+    exit 1
+fi
 
 HTTP_STATUS=$(curl -X GET 'http://localhost:9000/api/v2/produtos' -o product_list.json -w "%{http_code}" -H 'accept: */*')
 echo "Status HTTP: $HTTP_STATUS"
